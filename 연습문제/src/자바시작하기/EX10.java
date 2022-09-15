52bbc2a7dc530e6fb4372331a68efb3da8765485
dbqls0804 <113072902+dbqls0804@users.noreply.github.com>
2022-09-15 17:41:04 +0900

    220915_최초 커밋
    연습문제 10 추가

--- 연습문제/src/자바시작하기/EX10.java
+++ 연습문제/src/자바시작하기/EX10.java
@@ -5,8 +5,15 @@ public class EX10 {
 	public static void main(String[] args) {
 		// 연습문제 10
 		
+		int manwon = 10000;
+		manwon*=3;
+		int ochun = 5000;
+		ochun*=4;
+		int chun = 1000;
+		chun*=7;
 		
-
+		int total = manwon + ochun + chun;
+		
+		System.out.println(total + "원");
 	}
-
 }
--- 연습문제/src/자바시작하기/EX9.java
+++ 연습문제/src/자바시작하기/EX9.java
@@ -11,8 +11,5 @@ public class EX9 {
 		double sisok = totalkm / hr;
 		
 		System.out.println(sisok + " km/h");
-		
-		
 	}
-
 }
